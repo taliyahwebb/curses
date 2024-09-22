d2834eeae3a3b3f6649963acc46b77e47266843b
zqlk <zqlk@proton.me>
2024-09-23 00:38:18 +0200

    improved piper error messages

--- src-tauri/src/services/piper_tts/mod.rs
+++ src-tauri/src/services/piper_tts/mod.rs
@@ -1,6 +1,5 @@
-use futures::TryFutureExt;
 use serde::{Deserialize, Serialize};
-use std::{io, path::PathBuf};
+use std::{fmt::Display, io, path::PathBuf};
 use tauri::{plugin, Runtime};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -43,12 +42,26 @@ struct SpeakArgs {
     value: String,
 }
 
+/// Little helper extension to add a better error message to an io::Error
+trait ResultExt {
+    fn with_context<D: Display>(self, message: impl FnOnce() -> D) -> Self;
+}
+impl<T> ResultExt for io::Result<T> {
+    fn with_context<D: Display>(self, message: impl FnOnce() -> D) -> Self {
+        self.map_err(|e| {
+            let kind = e.kind();
+            let error = format!("{}: {}", message(), e);
+            io::Error::new(kind, error)
+        })
+    }
+}
+
 /// Scans the given directory for Piper voice files.
 /// A valid piper voice file must end in `.onnx` and have an accompanying `.onnx.json`
 fn scan_voice_directory(path: PathBuf) -> io::Result<Vec<Voice>> {
     fn try_into_voice(entry: std::fs::DirEntry) -> Option<Voice> {
         let path = entry.path();
-        if path.is_file() && path.extension().is_some_and(|ext| ext == "onnx") && path.with_extension("onnx.json").exists() {
+        if path.is_file() && path.extension() == Some("onnx".as_ref()) && path.with_extension("onnx.json").exists() {
             // we only need the name for display purposes, so the lossy conversion here is fine.
             let name = path.file_stem()?.to_string_lossy().into();
             Some(Voice { name, path })
@@ -67,9 +80,11 @@ fn scan_voice_directory(path: PathBuf) -> io::Result<Vec<Voice>> {
 async fn write_to_stdin(process: &mut tokio::process::Child, bytes: &[u8]) -> io::Result<()> {
     use tokio::io::AsyncWriteExt;
     let stdin = process.stdin.as_mut().expect("Failed to open stdin");
-    stdin.write_all(bytes).await
+    let result = stdin.write_all(bytes).await;
+    result.with_context(|| "Could not write to piper's stanard input")
 }
 
+/// Handles adding optional command line parameters to a Command object
 fn add_arg_if_some(command: &mut tokio::process::Command, name: &str, value: Option<impl ToString>) {
     if let Some(value) = value {
         command.arg(name);
@@ -77,11 +92,20 @@ fn add_arg_if_some(command: &mut tokio::process::Command, name: &str, value: Opt
     }
 }
 
+/// Tries to create a new temporary WAV file; Gives an error with some context if that fails.
+fn create_temp_file() -> io::Result<tempfile::NamedTempFile> {
+    let dir = tempfile::env::temp_dir();
+    tempfile::Builder::new()
+        .suffix(".wav")
+        .tempfile_in(&dir)
+        .with_context(|| format!("Failed to create temporary file in {}", dir.display()))
+}
+
 /// Invokes piper to generate a WAV file and returns it as a byte vector
-async fn get_wav_bytes(args: &SpeakArgs) -> Result<Vec<u8>, io::Error> {
+async fn get_wav_bytes(args: &SpeakArgs) -> io::Result<Vec<u8>> {
     // at first i tried to read the wav data from stdout, but that didn't work,
     // so now i just use a regular file here.
-    let wav_file = tempfile::NamedTempFile::new()?;
+    let wav_file = create_temp_file()?;
     let wav_file_path = wav_file.path();
 
     let mut command = tokio::process::Command::new(&args.exe_path);
@@ -107,7 +131,7 @@ async fn get_wav_bytes(args: &SpeakArgs) -> Result<Vec<u8>, io::Error> {
         command.creation_flags(0x08000000); // CREATE_NO_WINDOW
     }
 
-    let mut process = command.spawn()?;
+    let mut process = command.spawn().with_context(|| "Could not start Piper")?;
 
     write_to_stdin(&mut process, &args.value.as_bytes()).await?;
 
@@ -142,15 +166,16 @@ async fn speak(args: SpeakArgs) -> Result<(), String> {
         return Ok(());
     }
 
-    let bytes = get_wav_bytes(&args).map_err(|e| e.to_string()).await?;
+    let bytes = get_wav_bytes(&args).await.map_err(|e| e.to_string())?;
 
-    play_async(RpcAudioPlayAsync {
+    let play_async_args = RpcAudioPlayAsync {
         device_name: args.device,
         data: bytes,
         volume: 1.0,
         rate: 1.0,
-    })
-    .await
+    };
+
+    play_async(play_async_args).await
 }
 
 pub fn init<R: Runtime>() -> plugin::TauriPlugin<R> {
