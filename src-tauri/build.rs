dd9e6f9d0f92d9c1ad0478ca5b87f2225e77a441
pietagorh <pietagorhh@proton.me>
2025-03-02 07:33:27 +0100

    feat: add tauri permissions to plugin `web`

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,12 @@
 fn main() {
-    tauri_build::build()
+    tauri_build::try_build(
+        tauri_build::Attributes::new()
+            .plugin(
+                "web",
+                tauri_build::InlinedPlugin::new()
+                    .commands(&["open_browser", "pubsub_broadcast", "config"])
+                    .default_permission(tauri_build::DefaultPermissionRule::AllowAllCommands)
+            )
+    )
+    .expect("failed to run tauri-build");
 }
