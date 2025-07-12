f7e786cbab9843f43c18ee67f6bea58302601d3d
Pietagorh <pietagorhh@proton.me>
2025-07-12 10:11:28 +0200

    feat(inputfield): using the arrow keys to access past messages

--- src/server/ui/editor-view.tsx
+++ src/server/ui/editor-view.tsx
@@ -31,7 +31,7 @@ const EditorView: FC = () => {
           <ActionBar />
           <EditorViewport />
           <AnimatePresence initial={false}>
-            {!showOverlay && <div className="absolute flex justify-center self-center bottom-4 left-4 right-4"><STTInput /></div>}
+            {!showOverlay && <div className="absolute flex justify-center self-center bottom-4 left-4 right-4"><TextField /></div>}
           </AnimatePresence>
         </div>
         <AnimatePresence>
@@ -123,14 +123,21 @@ export const EditorViewport: FC = () => {
   </div>
 }
 
-const STTInput: FC = () => {
+const TextField: FC = () => {
   const { showLogs } = useSnapshot(window.ApiServer.state);
   const { t } = useTranslation();
+
   const [inputValue, setInputValue] = useState('');
+  const history = useSnapshot(window.ApiShared.pubsub.textHistory).list;
+  const [currentIndex, setCurrentIndex] = useState(history.length);
+
+
   const submit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (inputValue) {
       setInputValue('');
+      setCurrentIndex(history.length + 1);
+
       window.ApiShared.pubsub.publishText(TextEventSource.textfield, { type: TextEventType.final, value: inputValue });
     }
   }
@@ -138,6 +145,41 @@ const STTInput: FC = () => {
   const handleChange = (value: string) => {
     window.ApiShared.pubsub.publishText(TextEventSource.textfield, { type: TextEventType.interim, value });
     setInputValue(value);
+  }
+
+  const handleArrowKeys = (key: string, text: string, cursorPos: int) => {
+    switch (key) {
+      case "ArrowUp":
+        if (cursorPos !== 0) {
+          break;
+        }
+
+        if (currentIndex === 0) {
+          setInputValue("");
+          break;
+        }
+
+        setInputValue(history[currentIndex - 1].value);
+
+        // update after because the value will only actually get updated during the next render
+        setCurrentIndex(currentIndex - 1);
+        break;
+      case "ArrowDown":
+        if (cursorPos !== text.length) {
+          break;
+        }
+
+        if (currentIndex === history.length) {
+          setInputValue("");
+          break;
+        }
+
+        setInputValue(history[currentIndex + 1].value);
+
+        // update after because the value will only actually get updated during the next render
+        setCurrentIndex(currentIndex + 1);
+        break;
+      }
   }
 
   return <motion.div
@@ -149,7 +191,7 @@ const STTInput: FC = () => {
     className="flex items-center space-x-2 w-96">
     {/* <button className="btn btn-circle btn-ghost"><RiChatDeleteFill/></button> */}
     <form onSubmit={submit} className="w-full">
-      <input type="text" autoComplete="off" name="sttinput" placeholder={t('main.keyboard_input')} className="w-full input text-sm" value={inputValue} onChange={e => handleChange(e.target.value)} />
+      <input type="text" autoComplete="off" name="textfield" placeholder={t('main.keyboard_input')} className="w-full input text-sm" value={inputValue} onChange={e => handleChange(e.target.value)} onKeyDown={e => handleArrowKeys(e.key, e.target.value, e.target.selectionStart)} />
     </form>
   </motion.div>
 }
